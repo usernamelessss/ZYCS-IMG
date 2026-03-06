b602fa842e472623a6082dc1ff902e789bb4048d
usernameless <104410710+usernamelessss@users.noreply.github.com>
2026-03-07 06:56:46 +0800

    🐛 fix: 修复 bug 修改上传图片最大限制为 200M

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -57,7 +57,7 @@ const uploadAPI = ref<string>(`${import.meta.env.VITE_IMG_API_URL || location.or
 const UploadConfig = ref<any>({
   AcceptTypes: 'image/*', // 允许上传的类型，使用逗号分隔
   Max: 0, //多选个数，0为不限制
-  MaxSize: 15, //单个文件大小限制，单位：MB
+  MaxSize: 200, //单个文件大小限制，单位：MB
 });
 // 上传列表
 const fileList = ref<Array<any>>(JSON.parse(localStorage.getItem('zychUpImageList') || '[]'));
